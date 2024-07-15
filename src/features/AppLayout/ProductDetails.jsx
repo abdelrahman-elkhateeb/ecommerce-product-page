b50c91b97a28cb47a9ef6abc87d5d89cf53beb85
abdelrahman elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2024-07-15 17:17:26 +0300

    Update ProductDetails.jsx

--- src/features/AppLayout/ProductDetails.jsx
+++ src/features/AppLayout/ProductDetails.jsx
@@ -1,5 +1,16 @@
 function product() {
-  return <div></div>;
+  return (
+    <div>
+      <p>sneaker company</p>
+      <h1>fall limited edition sneakers</h1>
+      <p>
+        These low-profile sneakers are your perfect casual wear companion.
+        Featuring a durable rubber outer sole, they’ll withstand everything the
+        weather can offer.
+      </p>
+      <p></p>
+    </div>
+  );
 }
 
 export default product;
